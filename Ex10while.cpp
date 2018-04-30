8fd0ffb6e3143521fb7ce8482820f4233d7a3022
henriquelorenzini <38055951+henriquelorenzini@users.noreply.github.com>
2018-04-30 11:47:37 -0300

    Create Ex10while.cpp

---
+++ Ex10while.cpp
@@ -0,0 +1,32 @@
+*/ ta dando erro
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<locale.h>
+
+int main(){
+	setlocale(LC_ALL, "Portuguese");
+
+    int cont = 0,
+        num  = 0,
+        i    = 0,
+        j    = 1;
+
+    printf("Insira um número: ");
+    scanf("%d", &num);
+
+    while(cont= num){
+	
+		printf("%d", cont);
+        i++;
+	}
+        while(cont<=num) {
+            printf("\n");
+            j++;
+        	i = 0;
+        	cont++;
+        }
+        return 0;
+    }
+    
+	
